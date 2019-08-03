e89bd7e8a5369bbf19e708d62df445e6b8185dc2
Konrad Pluta <konplutar@gmail.com>
2019-08-03 15:32:33 +0200

    new looks

--- mxportal/settings.py
+++ mxportal/settings.py
@@ -140,7 +140,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = ''
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
 
 AUTHENTICATION_BACKENDS = (
     'django.contrib.auth.backends.ModelBackend',
